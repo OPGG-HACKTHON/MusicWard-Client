6af843ab3caf38558803a16c0304dd9113817d58
yeomhyeseon <hyeseon.yeom@meshkorea.net>
2021-08-16 21:15:42 +0900

    메인페이지 슬라이더 작업

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,6 +3,13 @@ import { useState } from "react";
 import styled from "styled-components";
 import BackgroundImg from "assets/img/background-img.png";
 import ChampionIcon from "assets/img/champion-icon.png";
+import WardIcon from "assets/img/ward-icon.png";
+import SliderPrevArrow from "assets/img/slider-prev-arrow.png";
+import SliderNextArrow from "assets/img/slider-next-arrow.png";
+import SliderSampleImg from "assets/img/slider-sample-img.png";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Main = () => {
   const [name] = useState("가렌 Garen");
@@ -10,18 +17,64 @@ const Main = () => {
   const [description] = useState(
     "가렌은 불굴의 선봉대를 이끄는 고결하고 자긍심 강한 전사다. 선봉대 내에서 인망이 두터울 뿐 아니라 심지어 적에게도 존경을 받지만, 그가 대대로 데마시아와 데마시아의 이상을 수호하는 임무를 맡은 크라운가드 가문의 자손이기 때문은 아니다. 가렌은 마법 저항력을 갖춘 방어구와 거대한 대검으로 무장하고, 언제라도 마법사에 맞서 정당한 검으로 진정한 칼바람을 일으킬 준비가 되어 있다"
   );
+  const [PlayList] = useState(
+    [...Array(15)].map((i, index) => {
+      return {
+        title: `데마시아의 힘을 느껴보자${index}`,
+        listCount: index,
+        wardCount: index * 10,
+      };
+    })
+  );
 
   return (
     <>
       <PlayListSection>
         <GradientSection />
         <TextWrapper>
-          <Icon src={ChampionIcon}></Icon>
+          <Icon src={ChampionIcon} width={34} height={34} />
           <Name>{name}</Name>
           <ShortInfo>{shortInfo}</ShortInfo>
           <Description>{description}</Description>
         </TextWrapper>
-        <SliderWrapper></SliderWrapper>
+        <SliderWrapper>
+          <Slider
+            className="slider variable-width"
+            dots
+            dotsClass="slick-dots slick-thumb"
+            infinite
+            speed={500}
+            slidesToShow={5}
+            slidesToScroll={5}
+            variableWidth
+            customPaging={(p) => <a>{p}</a>}
+            prevArrow={<SliderArrow type="prev" />}
+            nextArrow={<SliderArrow type="next" />}
+          >
+            {PlayList.map((i, index) => (
+              <SliderItem key={`slider-item-${index}`}>
+                <SliderItemBox>
+                  <SliderItemGradient>
+                    <ItemWrappder>
+                      <Title>{i.title}</Title>
+                      <CountWrapper>
+                        <Count>{i.listCount}곡</Count>
+                        <Divider />
+                        <Icon
+                          src={WardIcon}
+                          width={13}
+                          height={14}
+                          marginRight={2}
+                        />
+                        <Count>{i.wardCount}</Count>
+                      </CountWrapper>
+                    </ItemWrappder>
+                  </SliderItemGradient>
+                </SliderItemBox>
+              </SliderItem>
+            ))}
+          </Slider>
+        </SliderWrapper>
       </PlayListSection>
       <LaunchpadSection>
         <div></div>
@@ -34,6 +87,24 @@ const Main = () => {
 };
 
 export default Main;
+
+interface SliderArrowProps {
+  type: "next" | "prev";
+  className?: string;
+  style?: Record<string, string>;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+}
+
+const SliderArrow = ({ type, className, style, onClick }: SliderArrowProps) => {
+  return (
+    <img
+      className={className}
+      style={{ ...style, height: "30px" }}
+      onClick={onClick}
+      src={type === "prev" ? SliderPrevArrow : SliderNextArrow}
+    />
+  );
+};
 
 const PlayListSection = styled.section`
   height: 640px;
@@ -73,9 +144,14 @@ const TextWrapper = styled.div`
   top: 130px;
   left: 130px;
 `;
-const Icon = styled.img`
-  width: 34px;
-  height: 34px;
+const Icon = styled.img<{
+  width: number;
+  height: number;
+  marginRight?: number;
+}>`
+  width: ${({ width }) => `${width}px`};
+  height: ${({ height }) => `${height}px`};
+  margin-right: ${({ marginRight }) => `${marginRight || 0}px`};
 `;
 const Name = styled.div`
   font-size: 50px;
@@ -96,12 +172,86 @@ const Description = styled.div`
 const SliderWrapper = styled.div`
   height: 300px;
   width: 1220px;
-  background-color: #fff;
   position: absolute;
   z-index: 12;
-  bottom: -150px;
+  bottom: -175px;
   left: calc(50% - 610px);
 `;
+
+const SliderItem = styled.div`
+  position: relative;
+  width: 200px !important;
+  height: 200px;
+  background: conic-gradient(
+    #755c28,
+    #d3bf89,
+    #817347,
+    #433915,
+    #817347,
+    #d3bf89,
+    #755c28
+  );
+  padding: 12px;
+  box-sizing: border-box;
+  margin: 0 22px;
+`;
+
+const SliderItemBox = styled.div`
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  width: 190px;
+  height: 190px;
+  background-image: url(${SliderSampleImg});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const SliderItemGradient = styled.div`
+  position: absolute;
+  height: 130px;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  background: linear-gradient(
+    to bottom,
+    rgba(20, 20, 20, 0) 5%,
+    rgba(20, 20, 20, 0.25) 35%,
+    rgba(20, 20, 20, 0.5) 55%,
+    rgba(20, 20, 20, 0.75) 75%,
+    rgba(20, 20, 20, 1) 100%
+  );
+`;
+const ItemWrappder = styled.div`
+  position: absolute;
+  bottom: 15px;
+  right: 15px;
+  left: 15px;
+`;
+
+const Title = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+`;
+const CountWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 7px;
+`;
+const Count = styled.div`
+  font-size: 12px;
+  font-weight: medium;
+  opacity: 0.6;
+`;
+const Divider = styled.hr`
+  height: 13px;
+  width: 1px;
+  background-color: #666666;
+  opacity: 0.5;
+  margin: 0 10px;
+  border: 0;
+`;
+
 const LaunchpadSection = styled.section``;
 
 const LankingSection = styled.section``;
