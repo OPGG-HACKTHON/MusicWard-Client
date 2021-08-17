d70a9086efb61688aba0475835b1ea74602360bf
anyl92 <yulim9592@gmail.com>
2021-08-17 23:02:27 +0900

    유저쪽 태그들 정렬

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useState } from "react";
 import styled from "styled-components";
-import GoogleAccount from "assets/img/mypage/google-account.png";
+import PlayButton from "assets/img/mypage/play-button.png";
 import MyImgEdge from "assets/img/mypage/my-img-edge.png";
 import MyImgSample from "assets/img/mypage/my-img-sample.png";
+import GoogleAccount from "assets/img/mypage/google-account.png";
 
 const MyPage = () => {
   const [userId] = useState("와드깔고승리하자");
@@ -11,53 +12,90 @@ const MyPage = () => {
 
   return (
     <>
-      <MyImgBox>
-        <MyImg>
+      <MyBox>
+        <MyImgBox>
           <img
             style={{ width: "92%", height: "92%", margin: "4% 4%" }}
             src={MyImgSample}
             alt="user image"
           />
-        </MyImg>
-      </MyImgBox>
-      <MyInfoBox>
-        <img src={GoogleAccount} />
-        <div>{userId}</div>
-        <MyInfoLine />
-        <div>{userEmail}</div>
-      </MyInfoBox>
+        </MyImgBox>
+        <MyInfoBox>
+          <img style={{ width: "47px" }} src={PlayButton} />
+          <MyId>{userId}</MyId>
+          <MyInfoLine />
+          <MyInfoAccount>
+            <img
+              style={{ width: "23px", marginRight: "15px" }}
+              src={GoogleAccount}
+              alt="account icon"
+            />
+            <UserEmail>{userEmail}</UserEmail>
+          </MyInfoAccount>
+        </MyInfoBox>
+      </MyBox>
     </>
   );
 };
 
-const MyImgBox = styled.div`
-  width: 30%;
-  display: inline-block;
+const MyBox = styled.section`
+  height: 20vw;
   position: relative;
+  display: flex;
 `;
 
-const MyImg = styled.div`
+const MyImgBox = styled.section`
   width: 200px;
   height: 200px;
+  margin: auto 5vw auto 14vw;
   background-image: url(${MyImgEdge});
   background-repeat: no-repeat;
 `;
 
-const MyInfoBox = styled.div`
-  width: 70%;
-  display: inline-block;
+const MyInfoBox = styled.section`
   position: relative;
+  width: 60vw;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const MyId = styled.span`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 28px;
+  line-height: 41px;
+  color: #ffffff;
 `;
 
 const MyInfoLine = styled.hr`
   display: inline-block;
   width: 240px;
   height: 0px;
+  margin-left: 0;
 
   opacity: 0.8;
   /* gold/primary */
   border: 1px solid #bb8c3c;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+const MyInfoAccount = styled.div`
+  display: flex;
+  justify-content: left;
+`;
+
+const UserEmail = styled.div`
+  display: inline-block;
+
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
 `;
 
 export default MyPage;
