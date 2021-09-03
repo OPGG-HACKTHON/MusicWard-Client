27d304921cdd4e164e50eb3490213839e4108c76
yeomhyeseon <hyeseon.yeom@meshkorea.net>
2021-09-04 01:07:33 +0900

    localStorageEffect 정의

---
+++ src/utils/hooks/localStorageEffect.ts
@@ -0,0 +1,20 @@
+import { AtomEffect, DefaultValue } from "recoil";
+
+export const localStorageEffect: <T>(key: string) => AtomEffect<T> =
+  (key: string) =>
+  ({ setSelf, onSet }) => {
+    console.log(key);
+    console.log(localStorage.getItem(key));
+    const savedValue = localStorage.getItem(key);
+    if (savedValue !== null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue) => {
+      if (newValue instanceof DefaultValue) {
+        localStorage.removeItem(key);
+      } else {
+        localStorage.setItem(key, JSON.stringify(newValue));
+      }
+    });
+  };
