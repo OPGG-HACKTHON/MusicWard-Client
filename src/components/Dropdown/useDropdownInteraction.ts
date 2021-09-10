cf6d06ce4e146a2b3131eec8cd657eccdf0332f8
yeomhyeseon <hyeseon.yeom@meshkorea.net>
2021-09-10 23:08:59 +0900

    feat: dropdown 컴포넌트 추가

---
+++ src/components/Dropdown/useDropdownInteraction.ts
@@ -0,0 +1,70 @@
+import { useCallback, useState, useEffect, MutableRefObject } from "react";
+
+import { Option } from "./Dropdown";
+
+interface DropdownInteractionProps {
+  options: Option[];
+  node: MutableRefObject<HTMLDivElement | null>;
+  disabled?: boolean;
+  onChange: (v: any) => void;
+}
+
+const useDropdownInteraction = ({
+  disabled,
+  node,
+  onChange,
+}: DropdownInteractionProps) => {
+  const [isOpened, setIsOpened] = useState(false);
+
+  const handleToggleDropdown = useCallback(() => {
+    if (!disabled) {
+      setIsOpened(!isOpened);
+    }
+  }, [disabled, isOpened, setIsOpened]);
+
+  const handleClickOutsideDropdown = useCallback(
+    (e: any) => {
+      if (node.current && node.current.contains(e.target)) {
+        return;
+      }
+      setIsOpened(false);
+    },
+    [node]
+  );
+
+  const handleChange = useCallback(
+    (v: string) => {
+      onChange?.(v);
+      setIsOpened(false);
+    },
+    [onChange]
+  );
+
+  useEffect(() => {
+    if (node && node.current) {
+      window.addEventListener("mousedown", handleClickOutsideDropdown, false);
+
+      return () => {
+        window.removeEventListener(
+          "mousedown",
+          handleClickOutsideDropdown,
+          false
+        );
+      };
+    }
+  }, [node, handleClickOutsideDropdown]);
+
+  useEffect(() => {
+    if (disabled && isOpened) {
+      setIsOpened(false);
+    }
+  }, [disabled, isOpened, setIsOpened]);
+
+  return {
+    isOpened,
+    handleChange,
+    handleToggleDropdown,
+  };
+};
+
+export default useDropdownInteraction;
