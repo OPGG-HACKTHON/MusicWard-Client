2065b52ad3788b0a1ef28269740aa6cd49e0d050
yeomhyeseon <hyeseon.yeom@meshkorea.net>
2021-09-14 01:54:53 +0900

    feat: spotify 로그인

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,14 +8,15 @@ import PlayListPage from "pages/playListPage/PlayListPage";
 import { SearchCategory, SearchResultList } from "pages/searchPage";
 import { Redirect, Route, Switch } from "react-router-dom";
 import PrivateRoute from "router/PrivateRoute";
-import { useRecoilState } from "recoil";
-import { token, TokenType } from "recoil/auth";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { accessToken, token, TokenType } from "recoil/auth";
 import queryString from "query-string";
 import axiosInstance from "utils/axiosConfig";
 import Layout from "Layout";
 import AccountTermPage from "pages/etcPage/AccountTermPage";
 
 const Routes = () => {
+  const jwtToken = useRecoilValue(accessToken);
   const [, setToken] = useRecoilState<TokenType>(token);
   const parsed = queryString.parse(location.search);
   const getToken = async () => {
@@ -35,11 +36,35 @@ const Routes = () => {
     });
     localStorage.setItem("musicward_token", JSON.parse(data));
   };
+  const getSpotifyToken = async () => {
+    const { data } = await axiosInstance({
+      url: "users/auth/spotify",
+      method: "post",
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      params: {
+        code: parsed.code,
+      },
+    });
+    const { access_token, refresh_token, oauth_refresh_token, type } = data;
+    setToken({
+      accessToken: access_token,
+      refreshToken: refresh_token,
+      oauthRefreshToken: oauth_refresh_token,
+      type: type,
+    });
+    localStorage.setItem("musicward_token", JSON.parse(data));
+  };
   useEffect(() => {
+    if (parsed.code && jwtToken) {
+      getSpotifyToken();
+      return;
+    }
     if (parsed.code) {
       getToken();
     }
-  }, []);
+  }, [parsed]);
   return (
     <Layout>
       <Switch>
