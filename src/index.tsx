d0b344dd1e734a75aa51c85bba85b16177829cab
anyl92 <yulim9592@gmail.com>
2021-09-17 23:17:12 +0900

    feat: share kakao link

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+
+window.Kakao.init("324ce7ca3906aba5faa01ccd1cd5628e");
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
